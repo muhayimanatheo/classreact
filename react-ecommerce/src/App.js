37a3313c651dac82e7f0d4bb203e2058fce017e7
muhayimanatheo <muhayitheo23@gmail.com>
2024-02-22 10:22:26 +0200

    Layerout pages created

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router,Route,Routes, } from 'react-router-dom';
 import Home from './components/home';
 import ServicesFunc from './components/services';
 import BlogFunc from './components/blogs';
+import Parents from './components/parent';
 
 
 function App() {
@@ -11,9 +12,11 @@ function App() {
     
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} /> 
+        <Route path="/" element={<Parents />}>
+        <Route index element={<Home />} /> 
         <Route path="/blogs" element={<BlogFunc />} />
         <Route path="/services" element={<ServicesFunc />} />
+        </Route>
         </Routes>
       </Router>
   );
